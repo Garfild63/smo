b6c50e5c128d3e1cad192aa0c5e413acb6ce6a1c
Garfild63 <garfild63@list.ru>
2021-05-31 00:39:37 +0300

    Adding file github.bat

--- matrix.py
+++ matrix.py
@@ -4,12 +4,8 @@ class MatrixType:
 			self.mx = args[0] * [args[1] * [0]]
 			for i in range(args[0]):
 				self.mx[i] = self.mx[i].copy()
-			self.m = args[0]
-			self.n = args[1]
 		elif len(args) == 1:
 			self.mx = args[0]
-			self.m = self.height()
-			self.n = self.width()
 	
 	def __str__(self):
 		return str(self.mx)
@@ -21,15 +17,21 @@ class MatrixType:
 		return len(self.mx[0])
 
 	def withoutRowAndColumn(self, i, j):
-		mx = self.mx.copy()
-		mx.pop(i - 1)
-		for k in range(len(mx)):
-			mx[k] = mx[k].copy()
-			mx[k].pop(j - 1)
-		return MatrixType(mx)
+		if (i - 1) in range(self.height()) and (j - 1) in range(self.width()):
+			mx = self.mx.copy()
+			mx.pop(i - 1)
+			for k in range(self.height()):
+				mx[k] = mx[k].copy()
+				mx[k].pop(j - 1)
+			return MatrixType(mx)
+		else:
+			raise Error
 
 	def elementAt(self, i, j):
-		return self.mx[i - 1][j - 1]
+		if (i - 1) in range(self.height()) and (j - 1) in range(self.width()):
+			return self.mx[i - 1][j - 1]
+		else:
+			raise Error
 
 	def minor(self, i, j):
 		return self.withoutRowAndColumn(i, j).determinant()
@@ -38,28 +40,30 @@ class MatrixType:
 		return (-1) ** (i + j) * self.minor(i, j)
 
 	def determinant(self):
-		if self.width() == 1 and self.height() == 1:
+		if self.width() != self.height():
+			raise Error
+		if self.width() == 1:
 			s = self.elementAt(1, 1)
 		else:
 			s = 0
-			for j in range(1, self.width() + 1):
-				s = s + (self.algebraicComplement(1, j) * self.elementAt(1, j))
+			for j in range(self.width()):
+				s = s + (self.algebraicComplement(1, j + 1) * self.elementAt(1, j + 1))
 		return s
 
 	def transposed(self):
 		mx = self.width() * [self.height() * [0]]
-		for i in range(1, self.width() + 1):
-			mx[i - 1] = mx[i - 1].copy()
-			for j in range(1, self.height() + 1):
-				mx[i - 1][j - 1] = self.elementAt(j, i)
+		for i in range(self.width()):
+			mx[i] = mx[i].copy()
+			for j in range(self.height()):
+				mx[i][j] = self.elementAt(j + 1, i + 1)
 		return MatrixType(mx)
 
 	def union(self):
 		mx = self.width() * [self.height() * [0]]
-		for i in range(1, self.width() + 1):
-			mx[i - 1] = mx[i - 1].copy()
-			for j in range(1, self.height() + 1):
-				mx[i - 1][j - 1] = self.algebraicComplement(j, i)
+		for i in range(self.width()):
+			mx[i] = mx[i].copy()
+			for j in range(self.height()):
+				mx[i][j] = self.algebraicComplement(j + 1, i + 1)
 		return MatrixType(mx)
 		
 	def inverse(self):
@@ -68,27 +72,29 @@ class MatrixType:
 	def multiply(self, arg):
 		if isinstance(arg, int) or isinstance(arg, float):
 			mx = self.mx.copy()
-			for i in range(len(mx)):
+			for i in range(self.height()):
 				mx[i] = mx[i].copy()
-				for j in range(len(mx[i])):
+				for j in range(self.width()):
 					mx[i][j] = mx[i][j] * arg
 			return MatrixType(mx)
-		elif isinstance(arg, MatrixType) and self.width() == arg.height():
+		elif isinstance(arg, MatrixType):
+			if self.width() != arg.height():
+				raise Error
 			mx = self.height() * [arg.width() * [0]]
-			for i in range(1, self.height() + 1):
-				mx[i - 1] = mx[i - 1].copy()
-				for j in range(1, arg.width() + 1):
-					mx[i - 1][j - 1] = 0
-					for k in range(1, self.width() + 1):
-						mx[i - 1][j - 1] = mx[i - 1][j - 1] + self.elementAt(i, k) * arg.elementAt(k, j)
+			for i in range(self.height()):
+				mx[i] = mx[i].copy()
+				for j in range(arg.width()):
+					mx[i][j] = 0
+					for k in range(self.width()):
+						mx[i][j] = mx[i][j] + self.elementAt(i + 1, k + 1) * arg.elementAt(k + 1, j + 1)
 			return MatrixType(mx)
 		
 	def divide(self, arg):
 		if isinstance(arg, int) or isinstance(arg, float):
 			mx = self.mx.copy()
-			for i in range(len(mx)):
+			for i in range(self.height()):
 				mx[i] = mx[i].copy()
-				for j in range(len(mx[i])):
+				for j in range(self.width()):
 					mx[i][j] = mx[i][j] / arg
 			return MatrixType(mx)
 		elif isinstance(arg, MatrixType):
